6d279faed17f0e512e419e32122867fa606a07d3
Scott Burack <itsmescottb123@gmail.com>
2021-09-22 17:10:02 -0400

    completed timestampe and react article component

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -12,8 +12,8 @@ class Article extends React.Component {
         <div className="article-body">
           {this.props.description}
           <div className="article-meta-details">
-            <small>Created by: {this.props.author}, {this.props.created_at} ago,
-                  last updated: {this.props.updated_at} ago</small>  
+            <small>Created by: {this.props.author}, <Timestamp relative date={this.props.created_at} />,
+                  last updated: <Timestamp relative date={this.props.updated_at}/></small>  
           </div>
         </div>
       </React.Fragment>
