1cbd9bc8b3afdd84c658fda2c7b7f0186b408a33
Scott Burack <itsmescottb123@gmail.com>
2021-10-06 12:51:37 -0400

    pick color and set to background and input

--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -3,13 +3,30 @@ import PropTypes from "prop-types";
 import { SketchPicker } from "react-color";
 
 class Colorpicker extends React.Component {
-    render() {
-        return (
-          <React.Fragment>
-            <SketchPicker />
-          </React.Fragment>
-        );
+  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selector: props.selector
     }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = (color) => {
+    document.body.style.background = color.hex
+    let elem = document.querySelector('#' + this.state.selector)
+    elem.value = color.hex
+  }
+
+  render() {
+      return (
+        <React.Fragment>
+          <SketchPicker color={this.props.color} onChange={this.handleChange} />
+        </React.Fragment>
+      );
+  }
 }
 
 Colorpicker.propTypes = {}
